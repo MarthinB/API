34bdbd3b1eb69fadddadfaa981b688bd7e873aad
Marthin <35875859+MarthinB@users.noreply.github.com>
2025-03-17 08:24:33 +0100

    scheduled commit

--- BatchGit.bat
+++ BatchGit.bat
@@ -1,4 +1,7 @@
+echo off
 cd "C:\Users\Workstation\Desktop\API\Lemming\Chambers"
 start "C:\Users\Workstation\Desktop\API\Lemming\Chambers" TTS.DataStorage.Tab.Mod.exe
 cd "C:\Users\Workstation\Desktop\API\Lemming\Optod"
 start "C:\Users\Workstation\Desktop\API\Lemming\Optod" OPTOD_API.exe
+cd "C:\Users\Workstation\Desktop\API\Ormstrup\Buoy"
+start "C:\Users\Workstation\Desktop\API\Ormstrup\Buoy" Buoy_Ormstrup.exe

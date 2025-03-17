b1a1a491307d85d561b34cfacb3b2accdf245f56
Marthin <35875859+MarthinB@users.noreply.github.com>
2025-03-17 22:14:31 +0100

    scheduled commit

--- BatchGit2.bat
+++ BatchGit2.bat
@@ -1,4 +1,4 @@
-echo off
+cd "C:\Users\Workstation\Desktop\API"
 "C:\Program Files\Git\bin\git.exe" add .
 "C:\Program Files\Git\bin\git.exe" commit -m "scheduled commit"
 "C:\Program Files\Git\bin\git.exe" push

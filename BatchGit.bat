1ef810e94affeddb88cd4cab4b35824214e765f2
Marthin <35875859+MarthinB@users.noreply.github.com>
2025-04-26 13:47:34 +0200

    scheduled commit

--- BatchGit.bat
+++ BatchGit.bat
@@ -7,3 +7,5 @@ cd "C:\Users\Workstation\Desktop\API\Ormstrup\Buoy"
 start "C:\Users\Workstation\Desktop\API\Ormstrup\Buoy" Buoy_Ormstrup.exe
 cd "C:\Users\Workstation\Desktop\API\Ravnsoe\Nexsens_temp"
 start "C:\Users\Workstation\Desktop\API\Ravnsoe\Nexsens_temp" Buoy_Ravnsoe_Temp.exe
+cd "C:\Users\Workstation\Desktop\API\Ormstrup\AFC"
+start "C:\Users\Workstation\Desktop\API\Ormstrup\AFC" AFC_2_to_11.exe
